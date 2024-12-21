329cdc793f90b1793668e63cbbbc5ed76a1f94ec
Willian Caetano <caetano.willian@gmail.com>
2024-12-21 19:31:11 -0300

    Add user creation handler with request/response DTOs
    Introduce a new REST handler to manage user creation, including request and response DTOs for validation and formatting. This implementation integrates with the UserService to process incoming data and handle responses.

---
+++ app/user/entrypoint/rest/handler/handler.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"user-management/app/user/service"
+)
+
+type Handler struct {
+	userService service.UserService
+}
+
+func NewHandler(s service.UserService) *Handler {
+	return &Handler{userService: s}
+}
+
+func (h *Handler) CreateUser(c *gin.Context) {
+	var req CreateUserRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	inputUser := req.ToEntity()
+
+	user, err := h.userService.CreateUser(inputUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	response := ToResponse(user)
+
+	c.JSON(http.StatusOK, response)
+}
